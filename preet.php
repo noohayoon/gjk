deb324cb7c6211f23b2729b65a4f1e255a3c88e9
noohayoon <36268736+noohayoon@users.noreply.github.com>
2020-04-03 08:41:19 +0700

    preet.php

--- preet.php
+++ preet.php
@@ -11,7 +11,7 @@ echo " Time       : ".date('d-m-Y||H:i:s')." \n";
 echo color("green"," =================================== \n");
 
 //	function change(){
-        $nama = nama();p
+        $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
         ulang:
         echo color("nevy","?] Number : ");
@@ -77,7 +77,7 @@ echo color("green"," =================================== \n");
 						sleep(1);
 					}
 					sleep(3);
-			
+					$boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
 					$messageboba10 = fetch_value($boba10,'"message":"','"');
 					if(strpos($boba10, 'You can use this promo now...')){
 						echo "\n".color("green","+] Message: ".$messageboba10);
@@ -97,14 +97,14 @@ echo color("green"," =================================== \n");
 					$message1 = fetch_value($goride,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message1);
 							
-					echo "\n".color("yellow","!] Claim Voc COBAGOFOOD090320A");
+					echo "\n".color("yellow","!] Claim Voc COBAGOFOOD010420");
 					echo "\n".color("yellow","!] Please wait...");
 					for($a=1;$a<=3;$a++){
 						echo color("yellow",".");
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
 					sleep(3);
